2dd3ccdc0cd1211a093c2575bfe6e5853821f693
Larisa Pyykola <larisa.pyykola@gmail.com>
2022-10-27 11:34:57 +0300

    TopBar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import '../styles/App.scss';
+import TopBar from './TopBar'; 
 
 function App() {
   return (
     <div className="App">
-      Hello
-
+      <TopBar/>
     </div>
   );
 }
---
+++ src/components/TopBar.js
@@ -0,0 +1,13 @@
+import '../styles/navigation.scss'
+
+function TopBar() {
+    return (
+        <div className = "navigation">
+            <div className = "container">
+                <p>Memegram</p>
+            </div>
+        </div>
+    );
+}
+
+export default TopBar;
